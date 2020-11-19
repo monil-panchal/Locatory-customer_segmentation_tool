37cde490e0e6bbae51552a9f184120beeb8638ba
Ahsan Kamal <ah721730@dal.ca>
2020-11-19 01:57:42 -0400

    customer aut

--- locatory-app/index.py
+++ locatory-app/index.py
@@ -27,19 +27,19 @@ app.layout = html.Div([
               [Input('url', 'pathname')])
 def display_page(pathname):
     if pathname == '/':
-        if True:
+        if current_user.is_authenticated:
             return map_dashboard.layout
         else:
             return login.layout
 
     if pathname == '/map_dashboard':
-        if True:
+        if current_user.is_authenticated:
             return map_dashboard.layout
         else:
             return login.layout
 
     if pathname == '/other_dashboard':
-        if True:
+        if current_user.is_authenticated:
             return other_dashboard.layout
         else:
             return login.layout
@@ -52,7 +52,7 @@ def display_page(pathname):
             return login.layout
 
     if pathname == '/profile':
-        if True:
+        if current_user.is_authenticated:
             return profile.layout
         else:
             return login.layout
