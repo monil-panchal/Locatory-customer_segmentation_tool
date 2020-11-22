605abdf6f73eb7f2ffd5c86e2d15badb150379ef
aamir <am754815@dal.ca>
2020-11-22 14:06:48 -0400

    rfm in progress

--- locatory-backend/segmentation-service/app/api/rfm/endpoints.py
+++ locatory-backend/segmentation-service/app/api/rfm/endpoints.py
@@ -8,10 +8,12 @@ router = APIRouter()
 
 rfm_logger = Log.get_instance().root_logger
 
+
 @router.post("/rfm_segmentation_with_saved_data/", response_model=bool)
 async def rfm_segmentation_with_saved_data(document_id: str = Query(None, min_length=20, max_length=30)):
     rfm_logger.info("Request to perform RFM Segmentation")
     return True
+
 
 @router.post("/rfm_segmentation_with_parameters", response_model=bool)
 async def rfm_segmentation_with_parameters(rfm_parameters: RFMParameters):
--- locatory-backend/segmentation-service/app/api/rfm/rfm_parameters.py
+++ locatory-backend/segmentation-service/app/api/rfm/rfm_parameters.py
@@ -1,24 +1,26 @@
 from typing import Optional, List
 from pydantic import BaseModel, validator, Field
 
+
 class Demography(BaseModel):
     age_range: Optional[List[int]] = Field(
-            [],
-            min_items=2,
-            max_items=2
-            )
+        [],
+        min_items=2,
+        max_items=2
+    )
     gender: Optional[List[str]] = []
-    nationality: Optional[List[str]] = []
     income_range: Optional[List[float]] = Field(
-            [],
-            min_items=2,
-            max_items=2
-            )
+        [],
+        min_items=2,
+        max_items=2
+    )
+
 
 class Geography(BaseModel):
     country: Optional[List[str]] = []
     state: Optional[List[str]] = []
     city: Optional[List[str]] = []
+
 
 class RFMParameters(BaseModel):
     n_segments: int
