f32b5a9e7c7df25de40e005d2bd2345310306010
Monil Panchal <monil.panchal@dal.ca>
2020-11-23 21:10:39 -0400

    [Add] pie chart for sales by item category. Showing top 10 for both the pie charts

--- locatory-app/apps/views/graphs/sales_pie_chart.py
+++ locatory-app/apps/views/graphs/sales_pie_chart.py
@@ -4,20 +4,34 @@ import plotly.express as px
 
 
 def generate_pie_chart_by_location(current_df: pd.DataFrame, type: str):
-    fig = go.Figure()
+    df = current_df.nlargest(10, 'payment_value').copy()
+
+    fig = px.pie()
     print(f'pie chart type is: {type}')
 
     if type == 'country':
-        fig = px.pie(current_df, values=current_df['payment_value'],
-                     names=current_df['customer.address.customer_state'],
-                     title='Sales by state/province')
+        fig = px.pie(df, values=df['payment_value'],
+                     names=df['customer.address.customer_state'],
+                     title='Sales by state/province (top 10)')
     elif type == 'state':
-        fig = px.pie(current_df, values=current_df['payment_value'], names=current_df['customer.address.customer_city'],
-                     title='Sales by cities')
+        fig = px.pie(df, values=df['payment_value'], names=df['customer.address.customer_city'],
+                     title='Sales by cities (top 10)')
 
     elif type == 'city':
-        fig = px.pie(current_df, values=current_df['payment_value'], names=current_df['customer.address.zip_code'],
-                     title='Sales by area/zip_code')
+        fig = px.pie(df, values=df['payment_value'], names=df['customer.address.zip_code'],
+                     title='Sales by area/zip_code (top 10)')
+
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+
+    return fig
+
+
+def generate_pie_chart_by_product_category(current_df: pd.DataFrame):
+    df = current_df.nlargest(10, 'payment_value').copy()
+
+    fig = px.pie(df, values=df['payment_value'],
+                 names=df['product.category'],
+                 title='Sales by item category (top 10)')
 
     fig.update_traces(textposition='inside', textinfo='percent+label')
 
