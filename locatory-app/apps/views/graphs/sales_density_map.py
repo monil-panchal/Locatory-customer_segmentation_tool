4397aa2cbb428fb932b0acc923d022beba3d59d6
Monil Panchal <monil.panchal@dal.ca>
2020-11-24 02:50:00 -0400

    [add]: sales by region - using density mapbox

--- locatory-app/apps/views/graphs/sales_density_map.py
+++ locatory-app/apps/views/graphs/sales_density_map.py
@@ -18,13 +18,51 @@ def generate_density_map(current_df: pd.DataFrame):
 
     print(gdf.head())
 
-    # fig = px.density_mapbox(df, lat=df['lat-lng'].str[1], lon=df['lat-lng'].str[0], z=df['payment_value'], radius=10,
-    #                         mapbox_style="open-street-map")
-
-    fig = px.choropleth_mapbox(df, geojson=gdf["geometry"], color="payment_value",
-                               locations="lat-lng", featureidkey="properties.district",
-                               mapbox_style="carto-positron", zoom=9)
+    fig = px.density_mapbox(df, lat=df['lat-lng'].str[1], lon=df['lat-lng'].str[0], z=df['payment_value'], radius=15,
+                            mapbox_style="open-street-map", range_color=[0, df['payment_value'].max()])
     fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+    # fig = px.choropleth_mapbox(gdf, geojson=gdf.geometry, color="payment_value",
+    #                            locations="customer.address.customer_city", featureidkey="properties.district",
+    #                            mapbox_style="carto-positron", zoom=9)
+    # fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
+
+    # fig = go.Figure(data=go.Scattergeo(
+    #     lon=df['lat-lng'].str[0],
+    #     lat=df['lat-lng'].str[1],
+    #     # text=df['text'],
+    #     mode='markers',
+    #     marker=dict(
+    #         size=8,
+    #         opacity=0.8,
+    #         reversescale=True,
+    #         autocolorscale=False,
+    #         symbol='square',
+    #         line=dict(
+    #             width=1,
+    #             color='rgba(102, 102, 102)'
+    #         ),
+    #         colorscale='Blues',
+    #         cmin=0,
+    #         color=df['payment_value'],
+    #         cmax=df['payment_value'].max(),
+    #         colorbar_title="Incoming flights<br>February 2011"
+    #     )))
+    #
+    # fig.update_layout(
+    #     title='Most trafficked US airports<br>(Hover for airport names)',
+    #     geo=dict(
+    #         scope='south america',
+    #         # projection_type='albers usa',
+    #         showland=True,
+    #         landcolor="rgb(250, 250, 250)",
+    #         subunitcolor="rgb(217, 217, 217)",
+    #         countrycolor="rgb(217, 217, 217)",
+    #         countrywidth=0.5,
+    #         subunitwidth=0.5
+    #     ),
+    # )
+    # fig.show()
 
     # fig.show()
 
