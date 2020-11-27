79ade92942441b2246f415662ada292732fea6d3
Monil Panchal <monil.panchal@dal.ca>
2020-11-26 21:12:14 -0400

    [FIX] sales dashboard calender order

--- locatory-app/apps/dataframe/process_sales_dashboard_df.py
+++ locatory-app/apps/dataframe/process_sales_dashboard_df.py
@@ -2,6 +2,9 @@ import pandas as pd
 import datetime as dt
 import calendar
 
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+          "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
 
 def add_year_month_week(current_df: pd.DataFrame, previous_df: pd.DataFrame):
     if not current_df.empty:
@@ -10,10 +13,16 @@ def add_year_month_week(current_df: pd.DataFrame, previous_df: pd.DataFrame):
             current_df['month'] = current_df['month'].apply(lambda x: calendar.month_abbr[x])
             current_df['week'] = current_df['order_date'].apply(lambda d: (d.day - 1) // 7 + 1)
 
+            current_df['month'] = pd.Categorical(current_df['month'], categories=months, ordered=True)
+            current_df.sort_values('month')
+
     if not previous_df.empty:
         if 'order_date' in previous_df.columns:
             previous_df['month'] = previous_df['order_date'].dt.month
             previous_df['month'] = previous_df['month'].apply(lambda x: calendar.month_abbr[x])
             previous_df['week'] = previous_df['order_date'].apply(lambda d: (d.day - 1) // 7 + 1)
+
+            previous_df['month'] = pd.Categorical(previous_df['month'], categories=months, ordered=True)
+            previous_df.sort_values('month')
 
     return current_df, previous_df
