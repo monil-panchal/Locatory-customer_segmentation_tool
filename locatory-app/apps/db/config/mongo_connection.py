219b519929128eef5ce9c75a0239a7128d044683
Ahsan Kamal <ah721730@dal.ca>
2020-12-06 13:40:05 -0400

    load environment variable from .env

--- locatory-app/apps/db/config/mongo_connection.py
+++ locatory-app/apps/db/config/mongo_connection.py
@@ -3,20 +3,18 @@ import os
 from pymongo import MongoClient
 from urllib.parse import quote_plus
 
-dbname = os.environ.get('DB_NAME', "locatorydb")
-password = os.environ.get('DB_PASSWORD', "locatorydb@123")
-url = os.environ.get('DB_URL', "129.173.67.205")
-
 class PyMongo:
 
-    def __init__(self, dbname=dbname, password=password):
-        self.dbname = dbname
-        self.password = password
+    def __init__(self):
+        self.dbname = os.environ.get('DB_NAME', "locatorydb")
+        self.username = os.environ.get('DB_USERNAME', "admin")
+        self.password = os.environ.get('DB_PASSWORD', "locatorydb@123")
+        self.url = os.environ.get('DB_URL', "129.173.67.205")
         self.client = None
 
     def get_db_connection(self):
         self.client = MongoClient(
-            f"mongodb://{quote_plus('admin')}:{quote_plus(self.password)}@{url}:27017/{self.dbname}?authSource=admin")
+            f"mongodb://{quote_plus(self.username)}:{quote_plus(self.password)}@{self.url}:27017/{self.dbname}?authSource=admin")
         return self.client.get_database()
 
     def close_db_connection(self):
